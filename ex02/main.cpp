ca363aaa4b13decdc630bf5193f7d0a3902415eb
rk0730 <164816796+rk0730@users.noreply.github.com>
2025-03-25 19:02:14 +0900

    ex02 canonical form

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -9,5 +9,10 @@ int main(void)
 	fragtrap.beRepaired(5);
 	fragtrap.takeDamage(100);
 	fragtrap.attack("target");
+	
+	FragTrap fragtrap2 = fragtrap;
+	fragtrap2.attack("target");
+	FragTrap fragtrap3("Bob");
+	fragtrap3 = fragtrap2;
 	return 0;
 }
