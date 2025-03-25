537f982e5aacaaa09745ad51646763ecddbbfc74
rk0730 <164816796+rk0730@users.noreply.github.com>
2025-03-25 18:56:01 +0900

    ex01 canonical form

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -9,5 +9,10 @@ int main(void)
 	scavtrap.beRepaired(5);
 	scavtrap.takeDamage(100);
 	scavtrap.attack("target");
+	
+	ScavTrap scavtrap2(scavtrap);
+	scavtrap2.attack("target");
+	ScavTrap scavtrap3("Bob");
+	scavtrap3 = scavtrap2;
 	return 0;
 }
