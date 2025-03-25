8a12dcdf29eef0a98e69f9db9c7eadd57f126442
rk0730 <164816796+rk0730@users.noreply.github.com>
2025-03-25 22:39:28 +0900

    ex03 canonical form

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -8,6 +8,29 @@ DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), Scav
 	attack_damage = FragTrap::attack_damage;
 }
 
+DiamondTrap::DiamondTrap(const DiamondTrap &other) : ClapTrap(other.name + "_clap_name"), ScavTrap(other), FragTrap(other), name(other.name)
+{
+	std::cout << "DiamondTrap " << name << "(copy) is born!" << std::endl;
+	hit_points = FragTrap::hit_points;
+	energy_points = ScavTrap::energy_points;
+	attack_damage = FragTrap::attack_damage;
+}
+
+DiamondTrap &DiamondTrap::operator=(const DiamondTrap &other)
+{
+	if (this != &other)
+	{
+		ScavTrap::operator=(other);
+		FragTrap::operator=(other);
+		name = other.name;
+		hit_points = FragTrap::hit_points;
+		energy_points = ScavTrap::energy_points;
+		attack_damage = FragTrap::attack_damage;
+		std::cout << "DiamondTrap " << name << "(copy assign) is born!" << std::endl;
+	}
+	return *this;
+}
+
 DiamondTrap::~DiamondTrap()
 {
 	std::cout << "DiamondTrap " << name << " is dead!" << std::endl;
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -11,5 +11,11 @@ int main(void)
 	diamondtrap.beRepaired(5);
 	diamondtrap.takeDamage(100);
 	diamondtrap.attack("target");
+	
+	DiamondTrap diamondtrap2(diamondtrap);
+	diamondtrap2.whoAmI();
+	DiamondTrap diamondtrap3("Bob");
+	diamondtrap3 = diamondtrap2;
+	diamondtrap3.whoAmI();
 	return 0;
 }
